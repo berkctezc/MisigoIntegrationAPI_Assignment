648d656326d31baf6c934810b95df196ac373872
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-30 04:11:08 +0300

    manager implemented services

--- Business/Concrete/ProcedureManager.cs
+++ Business/Concrete/ProcedureManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Business.Abstract;
 using Core.Utilities.Results;
+using Entities.Abstract;
 using Entities.Concrete;
 
 namespace Business.Concrete
@@ -32,4 +33,222 @@ namespace Business.Concrete
             throw new System.NotImplementedException();
         }
     }
+
+    public class ProductManager : IProductService
+    {
+        public IResult Add(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Delete(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Update(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<List<Product>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<Product> GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class QueryManager : IQueryService
+    {
+        public IResult Add(Query query)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Delete(Query query)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Update(Query query)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<List<Query>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<Query> GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class RetailCustomerManager : IRetailCustomerService
+    {
+        public IResult Add(ICustomer customer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Delete(ICustomer customer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Update(ICustomer customer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<List<ICustomer>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<ICustomer> GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class RetailInvoiceManager : IRetailInvoiceService
+    {
+        public IResult Add(IInvoice invoice)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Delete(IInvoice invoice)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Update(IInvoice invoice)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<List<IInvoice>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<IInvoice> GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class RetailOrderManager : IRetailOrderService
+    {
+        public IResult Add(IOrder order)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Delete(IOrder order)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Update(IOrder order)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<List<IOrder>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<IOrder> GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }    
+    public class WholesaleCustomerManager : IWholesaleCustomerService
+    {
+        public IResult Add(ICustomer customer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Delete(ICustomer customer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Update(ICustomer customer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<List<ICustomer>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<ICustomer> GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class WholesaleInvoiceManager : IWholesaleInvoiceService
+    {
+        public IResult Add(IInvoice invoice)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Delete(IInvoice invoice)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Update(IInvoice invoice)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<List<IInvoice>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<IInvoice> GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class WholesaleOrderManager : IWholesaleOrderService
+    {
+        public IResult Add(IOrder order)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Delete(IOrder order)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IResult Update(IOrder order)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<List<IOrder>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDataResult<IOrder> GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
 }
