7ef2237a7c04d897cb3d5581b12346906c0848bc
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-30 17:47:58 +0300

    implemented retail order dtos

--- Business/Abstract/IRetailOrderService.cs
+++ Business/Abstract/IRetailOrderService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Core.Utilities.Results;
 using Entities.Concrete;
+using Entities.DTOs;
 
 namespace Business.Abstract
 {
@@ -10,6 +11,8 @@ namespace Business.Abstract
         IResult Delete(RetailOrder retailOrder);
         IResult Update(RetailOrder retailOrder);
         IDataResult<List<RetailOrder>> GetAll();
+        IDataResult<List<RetailOrderDto>> GetRetailOrderDetails();
         IDataResult<RetailOrder> GetById(int id);
+        IDataResult<RetailOrderDto> GetRetailOrderDetailsById(int id);
     }
 }
