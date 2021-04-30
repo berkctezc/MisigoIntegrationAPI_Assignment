c485b94ae4eee3b858f31c56d125b9ca2fd32431
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-30 17:59:59 +0300

    implemented wholesalecustomer dtos

--- Business/Abstract/IWholesaleCustomerService.cs
+++ Business/Abstract/IWholesaleCustomerService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Core.Utilities.Results;
 using Entities.Concrete;
+using Entities.DTOs;
 
 namespace Business.Abstract
 {
@@ -10,6 +11,8 @@ namespace Business.Abstract
         IResult Delete(WholesaleCustomer wholesaleCustomer);
         IResult Update(WholesaleCustomer wholesaleCustomer);
         IDataResult<List<WholesaleCustomer>> GetAll();
+        IDataResult<List<WholesaleCustomerDto>> GetWholesaleCustomerDetails();
         IDataResult<WholesaleCustomer> GetById(int id);
+        IDataResult<WholesaleCustomerDto> GetWholesaleCustomerDetailsById(int id);
     }
 }
