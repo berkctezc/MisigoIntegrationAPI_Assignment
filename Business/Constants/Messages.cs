b942975de6a3b2c452a195654e1b9b7f412062e8
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-30 13:39:18 +0300

    added essential crud operations on procedure manager and added messages

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,10 @@
+﻿namespace Business.Constants
+{
+    public class Messages
+    {
+        public static string ProcedureAdded = "Procedure Added";
+        public static string ProcedureUpdated = "Procedure Updated";
+        public static string ProcedureDeleted = "Procedure Deleted";
+        public static string ProceduresListed = "Procedures Listed";
+    }
+}
