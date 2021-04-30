911de93ad7d13c2163b60ec47355e60b9fe18a69
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-04-30 14:31:24 +0300

    added messages for wholesaleinvoice,customer and retailcustomer

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -13,6 +13,16 @@
         public static string WholesaleOrderUpdated = "Wholesale Order Updated";
         public static string WholesaleOrdersListed = "Wholesale Orders Listed";
         //
+        public static string WholesaleInvoiceAdded = "Wholesale Invoice Added";
+        public static string WholesaleInvoiceDeleted = "Wholesale Invoice Deleted";
+        public static string WholesaleInvoiceUpdated = "Wholesale Invoice Updated";
+        public static string WholesaleInvoicesListed = "Wholesale Invoices Listed";
+        //
+        public static string WholesaleCustomerAdded = "Wholesale Customer Added";
+        public static string WholesaleCustomerDeleted = "Wholesale Customer Deleted";
+        public static string WholesaleCustomerUpdated = "Wholesale Customer Updated";
+        public static string WholesaleCustomerListed = "Wholesale Customers Listed";
+        //
         public static string RetailOrderAdded = "Retail Order Added";
         public static string RetailOrderDeleted = "Retail Order Deleted";
         public static string RetailOrderUpdated = "Retail Order Updated";
@@ -22,6 +32,10 @@
         public static string RetailInvoiceDeleted = "Retail Invoice Deleted";
         public static string RetailInvoiceUpdated = "Retail Invoice Updated";
         public static string RetailInvoicesListed = "Retail Invoices Listed";
-
+        //
+        public static string RetailCustomerAdded = "Retail Customer Added";
+        public static string RetailCustomerDeleted = "Retail Customer Deleted";
+        public static string RetailCustomerUpdated = "Retail Customer Updated";
+        public static string RetailCustomersListed = "Retail Customers Listed";
     }
 }
