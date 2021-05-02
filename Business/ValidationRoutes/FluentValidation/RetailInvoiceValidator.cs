dd882bd153f8d710c736e471ec2133d2dc60be4d
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-02 16:10:30 +0300

    .

--- Business/ValidationRoutes/FluentValidation/RetailInvoiceValidator.cs
+++ Business/ValidationRoutes/FluentValidation/RetailInvoiceValidator.cs
@@ -8,9 +8,9 @@ namespace Business.ValidationRoutes.FluentValidation
     {
         public RetailInvoiceValidator()
         {
+            //incomplete
             Transform(from: ri => ri.ShipmentMethodCode, to: value => int.TryParse(value, out int val) ? (int?) val : null).NotEmpty().NotNull().GreaterThanOrEqualTo(1).LessThanOrEqualTo(3);
             RuleFor(rc => rc.PaymentTypeCode).NotEmpty().NotNull().GreaterThanOrEqualTo(1).LessThanOrEqualTo(5);
-
         }
     }
 }
