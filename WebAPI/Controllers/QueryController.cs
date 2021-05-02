14f4e76e28c2b4f77c7b5c181be8fccc624d68e4
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-02 17:13:09 +0300

    added query controller

---
+++ WebAPI/Controllers/QueryController.cs
@@ -0,0 +1,72 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QueryController : ControllerBase
+    {
+        IQueryService _queryService;
+        public QueryController(IQueryService queryService)
+        {
+            _queryService = queryService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _queryService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _queryService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Query query)
+        {
+            var result = _queryService.Add(query);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete(Query query)
+        {
+            var result = _queryService.Delete(query);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update(Query query)
+        {
+            var result = _queryService.Update(query);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
