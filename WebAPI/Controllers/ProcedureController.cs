72aaee63860d221430b225387632d4905bb484c8
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-02 16:11:39 +0300

    added controller for procedure with dtos

---
+++ WebAPI/Controllers/ProcedureController.cs
@@ -0,0 +1,95 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //attr
+    public class ProcedureController : ControllerBase
+    {
+        IProcedureService _procedureService;
+        public ProcedureController(IProcedureService procedureService)
+        {
+            _procedureService = procedureService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _procedureService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getalldetails")]
+        public IActionResult GetAllDetails()
+        {
+            var result = _procedureService.GetProcedureDetails();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _procedureService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getdetailsbyid")]
+        public IActionResult GetDetailsById(int id)
+        {
+            var result = _procedureService.GetProcedureDetailsById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpPost("add")]
+        public IActionResult Add(Procedure procedure)
+        {
+            var result = _procedureService.Add(procedure);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete(Procedure procedure)
+        {
+            var result = _procedureService.Delete(procedure);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update(Procedure procedure)
+        {
+            var result = _procedureService.Update(procedure);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
